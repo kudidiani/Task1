567aa88cc647d25587e37e732b4e2d1d3d7b5669
Diana <ku.didiani@gmail.com>
2022-08-30 21:41:35 +0300

    change to english

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 let x = {"k1":36.9, "k2":37.7, "k3":null, "k4":37.3, "k5":38.1, "k6":36.3};
-// console.log (x);
 for (let key in x) {
     if(x[key] === null){
-    console.log( "У ключа " + key + " значение " + x[key] );}
+    console.log( "the " + key + " key " + "is " + x[key] );}
   }
